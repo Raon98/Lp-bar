b50c598d1127aa139c119163b07192a18b2a17d0
Raon98 <cheol1998@kakao.com>
2024-06-27 17:49:40 +0900

    feat : 임시 메인화면 작업

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <>    
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      메인페이지
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between opacity-95">
+        <div className="relative w-full h-screen">
+          <Image
+            src="/assets/images/map0.jpg"
+            alt="Description of the image"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </main>
     </>
-);
+  );
 }
