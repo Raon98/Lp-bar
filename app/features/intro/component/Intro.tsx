ba18daa6defd93f9794fed5030cc46e2cdbf6d40
Raon98 <cheol1998@kakao.com>
2024-07-03 18:22:22 +0900

    feat : 이미지 리사이징 작업 및 폰트 추가

--- app/features/intro/component/Intro.tsx
+++ app/features/intro/component/Intro.tsx
@@ -2,24 +2,41 @@
 
 import MeteorEffectLayout from "@/app/common/component/MeteorEffectLayout";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Intro() {
+  const [useMounted, setUseMounted] = useState(false);
+
+  useEffect(() => {
+    setUseMounted(!useMounted);
+  }, []);
+
   return (
     <>
-      <div className="relative flex items-center z-10 h-full">
-        <div className="flex flex-col justify-center items-center text-2xl mb-80 text-white gap-5 w-1/2">
-          {["처", "리", "야", "잡", "화", "점"].map((char, index) => (
-            <span key={index}>{char}</span>
-          ))}
+      <div className="relative flex items-center z-10 h-full overflow-hidden">
+        <div className="w-1/2 z-10 flex justify-center">
+          <button className="cursor-pointer">
+            <div className="flex flex-col justify-center items-center text-2xl mb-80 text-white gap-5 ">
+              {["입", "장", "하", "기"].map((char, index) => (
+                <span key={index}>{char}</span>
+              ))}
+            </div>
+          </button>
         </div>
-        <div className="bg-transparent self-end relative w-screen h-1/2">
+        <div className="flex items-center justify-center bg-transparent flex-col self-end">
           <div className="sr-only">잡화점</div>
-          <Image
-            src="/assets/images/shop.png"
-            alt="shop"
-            layout="fill"
-            objectFit="cover"
-          />
+          <div className="flex flex-col text-white justify-end  text-7xl items-end w-full mr-[15rem]  text-">
+            <div>처리야</div>
+            <div>잡화점의</div>
+            <div>비밀</div>
+          </div>
+          {useMounted && (
+              <img
+                src="/assets/images/shop.png"
+                alt="shop"
+                className="bg-no-repeat bg-transparent bg-center object-cover"
+              ></img>
+          )}
         </div>
       </div>
       <MeteorEffectLayout />
