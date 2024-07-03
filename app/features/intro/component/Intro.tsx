a76ce0937f3dd49b96caeaa0f201d2518dc2c0c3
Raon98 <cheol1998@kakao.com>
2024-07-03 18:50:12 +0900

    feat: 입장하기 부분 버튼 사이즈 조절

--- app/features/intro/component/Intro.tsx
+++ app/features/intro/component/Intro.tsx
@@ -15,8 +15,8 @@ export default function Intro() {
     <>
       <div className="relative flex items-center z-10 h-full overflow-hidden">
         <div className="w-1/3 z-10 flex justify-center">
-          <button className="cursor-pointer">
-            <div className="flex flex-col justify-center items-center text-2xl mb-80 text-white gap-5 ">
+          <button className="cursor-pointer w-12 mb-80">
+            <div className="flex flex-col justify-center items-center text-3xl  text-white gap-5 ">
               {["입", "장", "하", "기"].map((char, index) => (
                 <span key={index}>{char}</span>
               ))}
@@ -25,17 +25,17 @@ export default function Intro() {
         </div>
         <div className="flex items-center justify-center bg-transparent flex-col self-end ml-[10rem] w-2/3">
           <div className="sr-only">잡화점</div>
-          <div className="flex flex-col text-white justify-end  text-[5rem] leading-[5rem]  items-end w-[65%]">
+          <div className="flex flex-col text-white justify-end sm:text-2xl  lg:text-[5.5rem] lg:leading-[5rem] leading-tight items-end w-[65%]">
             <div>처리야</div>
             <div>잡화점의</div>
             <div>비밀</div>
           </div>
           {useMounted && (
-              <img
-                src="/assets/images/shop.png"
-                alt="shop"
-                className="bg-no-repeat bg-transparent bg-center object-cover w-[55vw]"
-              ></img>
+            <img
+              src="/assets/images/shop.png"
+              alt="shop"
+              className="bg-no-repeat bg-transparent bg-center object-cover w-[55vw]"
+            ></img>
           )}
         </div>
       </div>
