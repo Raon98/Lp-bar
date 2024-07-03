47e2b0b8f8d6e46b5c5cf700299b4bc168bfb9c8
Raon98 <cheol1998@kakao.com>
2024-07-03 18:37:46 +0900

    fix : font 위치변경

--- app/features/intro/component/Intro.tsx
+++ app/features/intro/component/Intro.tsx
@@ -14,7 +14,7 @@ export default function Intro() {
   return (
     <>
       <div className="relative flex items-center z-10 h-full overflow-hidden">
-        <div className="w-1/2 z-10 flex justify-center">
+        <div className="w-1/3 z-10 flex justify-center">
           <button className="cursor-pointer">
             <div className="flex flex-col justify-center items-center text-2xl mb-80 text-white gap-5 ">
               {["입", "장", "하", "기"].map((char, index) => (
@@ -23,9 +23,9 @@ export default function Intro() {
             </div>
           </button>
         </div>
-        <div className="flex items-center justify-center bg-transparent flex-col self-end">
+        <div className="flex items-center justify-center bg-transparent flex-col self-end ml-[10rem] w-2/3">
           <div className="sr-only">잡화점</div>
-          <div className="flex flex-col text-white justify-end  text-7xl items-end w-full mr-[15rem]  text-">
+          <div className="flex flex-col text-white justify-end  text-[5rem] leading-[5rem]  items-end w-[65%]">
             <div>처리야</div>
             <div>잡화점의</div>
             <div>비밀</div>
@@ -34,7 +34,7 @@ export default function Intro() {
               <img
                 src="/assets/images/shop.png"
                 alt="shop"
-                className="bg-no-repeat bg-transparent bg-center object-cover"
+                className="bg-no-repeat bg-transparent bg-center object-cover w-[55vw]"
               ></img>
           )}
         </div>
