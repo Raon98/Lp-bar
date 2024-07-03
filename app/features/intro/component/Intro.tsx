d022871d131afc72d01a0f1dcb1065434c8bd0f3
Raon98 <cheol1998@kakao.com>
2024-07-03 10:40:27 +0900

    refactor : shop 이미지변경

--- app/features/intro/component/Intro.tsx
+++ app/features/intro/component/Intro.tsx
@@ -7,21 +7,20 @@ export default function Intro() {
   return (
     <>
       <div className="relative flex items-center z-10 h-full">
-        {/* <div className="flex flex-col justify-center items-center text-2xl mb-80 text-white gap-5 w-1/2">
+        <div className="flex flex-col justify-center items-center text-2xl mb-80 text-white gap-5 w-1/2">
           {["처", "리", "야", "잡", "화", "점"].map((char, index) => (
             <span key={index}>{char}</span>
           ))}
         </div>
-        <div className="bg-transparent self-end">
+        <div className="bg-transparent self-end relative w-screen h-1/2">
           <div className="sr-only">잡화점</div>
           <Image
             src="/assets/images/shop.png"
             alt="shop"
-            width={700}
-            height={500}
+            layout="fill"
             objectFit="cover"
           />
-        </div> */}
+        </div>
       </div>
       <MeteorEffectLayout />
     </>
