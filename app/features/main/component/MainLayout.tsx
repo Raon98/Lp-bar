d427db5a59946d0d9fc76583e26d0494acf8510e
Raon98 <cheol1998@kakao.com>
2024-07-04 22:10:44 +0900

    refactor : 이미지 변경 및 이미지 레이아웃 재정비

--- app/features/main/component/MainLayout.tsx
+++ app/features/main/component/MainLayout.tsx
@@ -26,11 +26,11 @@ export default function MainLayout() {
               alt="shop"
               className="absolute left-0 right-0 top-0 bottom-0 bg-no-repeat bg-transparent bg-center object-cover w-screen h-screen "
             />
-            <div className=" relative flex justify-center items-center w-full h-screen">
+            <div className=" relative flex justify-center items-center w-full h-full select-none">
               <img
-                src="/assets/images/carpet.jpg"
+                src="/assets/images/carpet.png"
                 alt="shop"
-                className="bg-no-repeat bg-transparent bg-center object-cover w-[80vw]"
+                className="bg-no-repeat bg-transparent bg-center object-cover w-[70vw] block opacity-95"
               ></img>
             </div>
           </div>
