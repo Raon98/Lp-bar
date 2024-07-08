56e6af18374367f1112c83cb2aa62a1a93f3793e
Raon98 <cheol1998@kakao.com>
2024-07-08 16:02:15 +0900

    feat: 메인페이지 전체 테마 임시 등록

--- app/features/main/component/MainLayout.tsx
+++ app/features/main/component/MainLayout.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { cn } from "@/app/common/utils/cn";
+import { useState } from "react";
+
 export default function MainLayout() {
 
-
+  const [theme,setTheme] = useState('bg-matte-red')
   return (
     <>
-          <div className="w-full h-full overflow-hidden relative">
+          <div className={cn(`w-full h-full overflow-hidden relative px-8 py-5 ${theme}`,'')}>
            
           </div>
     </>
