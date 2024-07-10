171f7b6da34cdb98cf58efe489ed0961dc363065
Raon98 <cheol1998@kakao.com>
2024-07-10 20:59:11 +0900

    feat : 메인페이지 이미지 교체 및 레이아웃 수정

--- app/features/main/component/MainLayout.tsx
+++ app/features/main/component/MainLayout.tsx
@@ -14,22 +14,17 @@ export default function MainLayout() {
         )}
       >
         <div className="flex w-full h-screen">
-          <div className="w-[70%]">
-            <div className="sr-only">레코드판</div>
+          <div className="sr-only">레코드판</div>
 
-            <div className="w-full h-full flex items-center justify-center">
-              <div className="relative">
-                <img
-                  src="/assets/images/turnTable.png"
-                  alt="recode"
-                  className="bg-no-repeat bg-transparent bg-center object-cover w-full"
-                ></img>
-                <span className="before:absolute before:top-0 before:right-0 before:content-[' '] before:bg-[url('/assets/images/toneArm.png')] before:bg-center before:bg-no-repeat before:w-[20%] before:h-full before:transform before:translate-x-[-100%] before:translate-y-[-10%] before:bg-contain"></span>
-              </div>
+          <div className="w-full h-full flex items-center justify-center">
+            <div className="relative w-1/2  ">
+              <img
+                src="/assets/images/turnTable.png"
+                alt="recode"
+                className="bg-no-repeat bg-transparent bg-center object-cover w-full"
+              ></img>
+              <span className="before:absolute before:top-0 before:right-0 before:content-[' '] before:bg-[url('/assets/images/toneArm.png')] before:bg-center before:bg-no-repeat before:w-[20%] before:h-full before:transform before:translate-x-[-120%] before:translate-y-[-15%] before:bg-contain"></span>
             </div>
-          </div>
-          <div className="w-[30%]">
-            <div className="sr-only">Lp판</div>
           </div>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,6 +80,10 @@ const config: Config = {
         scale : {
           '0%' : {transform :'scale(1)' }, 
           '100%' : {transform :'scale(10)'}, 
+        },
+        mainArmSpin : {
+          '0%' : { transform : 'translate(-120%, -12%)'},
+          '100%' : { transform : 'translate(-120%, -12%) rotate(45deg)'},
         }
       },
       animation : {
@@ -91,6 +95,7 @@ const config: Config = {
         'switchStop' : 'switchStop 0.5s linear 1 0s normal forwards',
         'introRecodeUp' : 'recodeUp 1.3s linear 1 0s normal forwards',
         'introScale' : 'scale 1s linear 1 0s normal forwards',
+        'mainArmSpin' : 'mainArmSpin 0.5s linear 1 0s normal forwards'
 
       }
     },
