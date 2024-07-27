65b65958a37e8ec3e7ae7e7d63e278665f7e677f
Raon98 <cheol1998@kakao.com>
2024-07-27 14:56:07 +0900

    refactor : 이력서 목차 제거

--- app/features/index/component/IndexComponent.tsx
+++ app/features/index/component/IndexComponent.tsx
@@ -1,20 +1,16 @@
-import { useTheme } from "@/app/hooks/themeContext";
-import ResumeIndex from "./ResumeIndex";
 import AccompenyIndex from "./AccompenyIndex";
 import PolaroidIndex from "./PolaroidIndex";
 
 interface IndexComponentProp {
-  id : string
+  id: string;
 }
 
-const IndexComponent = ({id} : IndexComponentProp) => {
-
-  console.log(id)
+const IndexComponent = ({ id }: IndexComponentProp) => {
+  console.log(id);
   return (
     <>
-      {id === '1' && <ResumeIndex/>}
-      {id === '2' && <AccompenyIndex/>}
-      {id === '3' && <PolaroidIndex/>}
+      {id === "2" && <AccompenyIndex />}
+      {id === "3" && <PolaroidIndex />}
     </>
   );
 };
