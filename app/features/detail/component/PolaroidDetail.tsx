821b7eb6ea5d4a56ae85b7ce5c5a2d5275404e64
Raon98 <cheol1998@kakao.com>
2024-07-31 20:32:07 +0900

    fix : 화면크기에 따라 첫번째 top height값을 빼도록 로직 수정

--- app/features/detail/component/PolaroidDetail.tsx
+++ app/features/detail/component/PolaroidDetail.tsx
@@ -1,52 +1,68 @@
 "use client";
-import { useTheme } from "@/app/hooks/themeContext";
 import useSectionStore from "@/app/store/useSectionStore";
 import { useEffect, useRef } from "react";
 import TabSectionContainer from "../../section/TabSectionContainer";
-import PolaroidIntroduce from "../../section/polaroid/PolaroidIntroduce";
 import PolaroidFeatures from "../../section/polaroid/PolaroidFeatures";
-import PolaroidTechStack from "../../section/polaroid/PolaroidTechStack";
 import PolaroidInSight from "../../section/polaroid/PolaroidInSight";
+import PolaroidIntroduce from "../../section/polaroid/PolaroidIntroduce";
+import PolaroidTechStack from "../../section/polaroid/PolaroidTechStack";
 
 const PolaroidDetail = () => {
-  const { getTabList, setSectionHeight,setInitTab } = useSectionStore();
+  const { getTabList, setSectionHeight, setInitTab } = useSectionStore();
   const sectionRefs = useRef<HTMLDivElement[]>([]);
 
   useEffect(() => {
-    setInitTab(()=> {
-
+    setInitTab(() => {
       if (sectionRefs.current) {
+        const firstTop = sectionRefs.current[0].getBoundingClientRect().top;
+
         sectionRefs.current.forEach((ref, idx) => {
           if (ref) {
             const rect = ref.getBoundingClientRect();
+
             const elBottom = rect.bottom + window.scrollY;
-            const elTop = ref.offsetTop - 325;
-            setSectionHeight(idx, elTop,elBottom);
+            const elTop = ref.offsetTop - firstTop;
+            setSectionHeight(idx, elTop, elBottom);
           }
         });
       }
+      console.log(getTabList());
     });
- 
-
   }, []);
 
   return (
     <>
       <div className="w-full">
         <TabSectionContainer sectionIdx={0}>
-          <PolaroidIntroduce ref={(e) => { if (e) sectionRefs.current[0] = e;}}/>
+          <PolaroidIntroduce
+            ref={(e) => {
+              if (e) sectionRefs.current[0] = e;
+            }}
+          />
         </TabSectionContainer>
 
         <TabSectionContainer sectionIdx={1}>
-          <PolaroidFeatures ref={(e) => { if (e) sectionRefs.current[1] = e;}}/>
+          <PolaroidFeatures
+            ref={(e) => {
+              if (e) sectionRefs.current[1] = e;
+            }}
+          />
         </TabSectionContainer>
 
         <TabSectionContainer sectionIdx={2}>
-          <PolaroidTechStack ref={(e) => { if (e) sectionRefs.current[2] = e;}}/>
-          </TabSectionContainer>
+          <PolaroidTechStack
+            ref={(e) => {
+              if (e) sectionRefs.current[2] = e;
+            }}
+          />
+        </TabSectionContainer>
 
         <TabSectionContainer sectionIdx={4}>
-          <PolaroidInSight ref={(e) => { if (e) sectionRefs.current[4] = e;}}/>
+          <PolaroidInSight
+            ref={(e) => {
+              if (e) sectionRefs.current[4] = e;
+            }}
+          />
         </TabSectionContainer>
       </div>
     </>
