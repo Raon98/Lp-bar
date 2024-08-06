fa8b0529597e5d1d09e514517deb158149d47ffb
Raon98 <cheol1998@kakao.com>
2024-08-06 13:08:56 +0900

    feat : accompany intro 추가

--- app/features/section/accompany/AccompanyIntroduce.tsx
+++ app/features/section/accompany/AccompanyIntroduce.tsx
@@ -6,16 +6,37 @@ const AccompanyIntroduce = forwardRef<HTMLDivElement>((_, sectionRefs) => {
       <div className="flex flex-col" ref={sectionRefs}>
         <div className="text-[2.275rem]">동행하다</div>
         <div className="text-[1.275rem]">태어난 순간부터 나와 늘 동행하는 사람</div>
-        <div className="flex  mt-[7.5rem] items-center  justify-around">
-          <div className="w-[15.5rem] h-[15.5rem]  shadow-custom-border-3 flex items-center justify-center flex-col rounded-md bg-white">
+        <div className="flex  mt-[7.5rem] items-center  justify-center gap-x-12">
+          <div className="w-[20.5rem] h-[20.5rem]  shadow-custom-border-3 flex items-center justify-center flex-col rounded-md bg-white">
+            <div className="w-[6.5rem] h-[6.5rem] mb-10 flex items-center justify-center">
+                <img
+                        src={`/assets/images/accompany_intro1.png`}
+                        alt="icon"
+                        className="bg-no-repeat bg-transparent bg-center object-cover w-[85%]"
+                      />
+            </div>
             <div>당신이 태어난 순간</div>
             <div>어떤 분들이 축하해주었나요?</div>
           </div>
-          <div className="w-[15.5rem] h-[15.5rem]  shadow-custom-border-3 flex items-center justify-center flex-col rounded-md bg-white">
+          <div className="w-[20.5rem] h-[20.5rem]  shadow-custom-border-3 flex items-center justify-center flex-col rounded-md bg-white">
+          <div className="w-[6.5rem] h-[6.5rem] mb-10">
+                <img
+                        src={`/assets/images/accompany_intro2.png`}
+                        alt="icon"
+                        className="bg-no-repeat bg-transparent bg-center object-cover w-full"
+                      />
+            </div>
             <div>당신의 결혼하는 순간</div>
             <div>어떤 분들이 축복해주었나요?</div>
           </div>
-          <div className="w-[15.5rem] h-[15.5rem]  shadow-custom-border-3 flex items-center justify-center flex-col rounded-md bg-white">
+          <div className="w-[20.5rem] h-[20.5rem]  shadow-custom-border-3 flex items-center justify-center flex-col rounded-md bg-white">
+          <div className="w-[6.5rem] h-[6.5rem] mb-10 bg-black rounded-full">
+                <img
+                        src={`/assets/images/accompany_intro3.png`}
+                        alt="icon"
+                        className="bg-no-repeat bg-transparent bg-center object-cover w-full mt-3"
+                      />
+            </div>
             <div>당신의 가장 가까운사람이 떠나갈때</div>
             <div>어떤 분들이 곁에서 위로를 해주었나요?</div>
           </div>
