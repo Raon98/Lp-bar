aff879b61249bff7c9318e40dbc7bff53808669a
Raon98 <cheol1998@kakao.com>
2024-08-08 23:27:28 +0900

    feat : 아이콘 array로 받도록 수정

--- app/features/section/accompany/AccompanyTechStack.tsx
+++ app/features/section/accompany/AccompanyTechStack.tsx
@@ -4,8 +4,8 @@ import { forwardRef } from "react";
 const AccompanyTechStack = forwardRef<HTMLDivElement>((_, sectionRefs) => {
   return (
     <div className="flex flex-col px-6 py-8 gap-y-3 " ref={sectionRefs}>
-      <TechIconComponent title="FRONTEND" />
-      <TechIconComponent title="BACKEND" />
+      <TechIconComponent title="FRONTEND" icon={["react", "Typescript"]} />
+      <TechIconComponent title="BACKEND" icon={["java"]} />
     </div>
   );
 });
