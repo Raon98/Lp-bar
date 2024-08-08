f6696211401ce48f94e877cb4b45c06f31b04298
Raon98 <cheol1998@kakao.com>
2024-08-09 00:06:51 +0900

    feat : TechComponent row Test

--- app/features/section/accompany/AccompanyTechStack.tsx
+++ app/features/section/accompany/AccompanyTechStack.tsx
@@ -3,9 +3,14 @@ import { forwardRef } from "react";
 
 const AccompanyTechStack = forwardRef<HTMLDivElement>((_, sectionRefs) => {
   return (
-    <div className="flex flex-col px-6 py-8 gap-y-3 " ref={sectionRefs}>
-      <TechIconComponent title="FRONTEND" icon={["React", "Typescript"]} />
-      <TechIconComponent title="BACKEND" icon={["java"]} />
+    <div
+      className="flex justify-center px-6 py-8 gap-y-3 w-full"
+      ref={sectionRefs}
+    >
+      <div className="flex">
+        <TechIconComponent title="FRONTEND" icon={["React", "Typescript"]} />
+        <TechIconComponent title="BACKEND" icon={["java"]} />
+      </div>
     </div>
   );
 });
