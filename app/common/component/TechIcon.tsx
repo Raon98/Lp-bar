aeee43dd11a6ad4a4b78131d72ab2557c6729828
Raon98 <cheol1998@kakao.com>
2024-08-16 14:59:39 +0900

    feat: Icon 상속관계 수정

--- app/common/component/TechIcon.tsx
+++ app/common/component/TechIcon.tsx
@@ -3,19 +3,21 @@ import Image from "next/image";
 type FrontnedIcon = "React" | "Typescript";
 type BackendIcon = "Java";
 
-export type IconType = (FrontnedIcon | BackendIcon)[];
+type Extention = "svg"| "png" | "jpg"
+type IconType = (FrontnedIcon | BackendIcon)[];
 
-interface TechIconProps {
+ export interface TechIconProps {
   icon: IconType;
+  extension? : Extention;
 }
-const TechIcon = ({ icon }: TechIconProps) => {
+const TechIcon = ({ icon,extension }: TechIconProps) => {
   return (
     <>
       {icon?.length > 0 &&
         icon.map((icon) => (
           <div className="min-w-[4rem] min-h-[4rem] max-w-[4rem] max-h-[4rem] rounded-lg shadow-custom-border-3 flex justify-center items-center">
             <Image
-              src={`/assets/images/${icon}.png`}
+              src={`/assets/images/${icon}.${extension || "png"}`}
               width={100}
               height={100}
               alt="icon"
--- app/features/detail/component/TechIconComponent.tsx
+++ app/features/detail/component/TechIconComponent.tsx
@@ -1,17 +1,17 @@
-import TechIcon, { IconType } from "@/app/common/component/TechIcon";
+import TechIcon, { TechIconProps } from "@/app/common/component/TechIcon";
 
-interface TechProps {
+interface TechProps extends TechIconProps {
   title: string;
-  icon: IconType;
 }
-const TechIconComponent = ({ title, icon }: TechProps) => {
+
+const TechIconComponent = ({ title, icon, extension }: TechProps) => {
   return (
-    <div className="h-[12rem] m-6  border-solid border-gray-300 border relative rounded-lg min-w-[20rem]">
-      <div className=" absolute left-0 top-0 text-[1.715rem] translate-x-[2rem] translate-y-[-1.5rem] bg-white ">
+    <div className="h-[12rem] m-6  border-solid border-gray-300 border relative rounded-lg ">
+      <div className=" absolute left-0 top-0 text-[1.215rem] translate-x-[2rem] translate-y-[-0.85rem] bg-white ">
         {title}
       </div>
       <div className="py-8 px-6 flex items-center h-full gap-x-3 gap-y-2 flex-wrap">
-        <TechIcon icon={icon} />
+        <TechIcon icon={icon} extension={extension}/>
       </div>
     </div>
   );
