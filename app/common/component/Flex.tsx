4fdc0339b75fb21db629894d79188e08eff07b3a
Raon98 <cheol1998@kakao.com>
2024-11-14 15:22:22 +0900

    fix : flex 공용컴포넌트 수정

--- app/common/component/Flex.tsx
+++ app/common/component/Flex.tsx
@@ -1,25 +1,32 @@
-"use client";
+'use client'
 
 interface FlexProps {
-  children: React.ReactNode;
-  direction?: string;
-  justify?: string;
-  align?: string;
-  className?: string; 
+  children: React.ReactNode
+  direction?: 'row' | 'row-reverse' | 'col' | 'col-reverse'
+  justify?: 'start' | 'center' | 'end' | 'between' | 'around' | 'evenly'
+  align?: 'start' | 'center' | 'end' | 'stretch' | 'baseline'
+  gap?: number
+  className?: string
 }
 
 export default function Flex({
   children,
-  direction = "flex-row",
-  justify = "justify-center",
-  align = "items-center",
-  className = "", 
+  direction = 'row',
+  justify = 'start',
+  align = 'stretch',
+  gap= 0,
+  className = '',
 }: FlexProps) {
+  const directionClass = direction ? `flex-${direction}` : ''
+  const justifyClass = justify ? `justify-${justify}` : ''
+  const alignClass = align ? `items-${align}` : ''
+  const gapClass = gap ? `gap-${gap}` : 0
+
   return (
     <div
-      className={`flex w-full h-full ${direction} ${justify} ${align} ${className}`} 
+      className={`flex ${directionClass} ${justifyClass} ${alignClass} ${gapClass} ${className}`}
     >
       {children}
     </div>
-  );
+  )
 }
